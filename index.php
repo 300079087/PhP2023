b66ab0819467c675cf2cb891237804e62a284245
austin <300079087@fvtc.edu>
2023-01-25 20:48:46 -0600

    "testing comment commit"

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ajs.homepage</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+<body>
+<div id="wrapper">
+    <header>
+    <h1>Austins Website</h1>
+    </header>
+    <nav>
+    <ul>
+        <li><a href="/">Homepage Link</a></li>
+        <li><a href="#">Loops Demo</a></li>
+        <li><a href="#">Countdown Timer</a></li
+    </ul>
+    </nav>
+    <main>
+        <h1>Who is Austin Steffes?</h1>
+        <img src="images/germanshep.jpg" alt="Austin Steffes" height="200" width="200" />
+        <p>Hello, my name is Austin Steffes a 19 year old student in the software development program. A novice, learning programming a day at a time.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, iure unde. A architecto cumque dicta dolor ducimus eius, error et eum ex exercitationem expedita fugiat incidunt labore libero magni, minima nihil porro qui quo sit suscipit veritatis vero! Animi doloribus facilis illo nisi officiis quae quidem sed suscipit, tenetur voluptatibus.</p>
+    </main>
+    <footer>
+    &copy; <?date("y")?>, Austin Steffes
+    </footer>
+</div>
+</body>
+</html>
